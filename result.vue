0e891005d5514341fc0af8a47adfcaf9e730f182
Martensit2019 <55350443+Martensit2019@users.noreply.github.com>
2021-11-17 00:30:07 +0300

    Create result.vue

---
+++ result.vue
@@ -0,0 +1,7 @@
+startTime(){
+      let endTime = this.result.date.split(" ")[1]
+      endTime = endTime.split(':').reduce((acc,time) => (60 * acc) + +time)
+      let startTime = endTime - this.result.elapsed_time
+      startTime = (new Date(startTime * 1000)).toUTCString().match(/(\d\d:\d\d:\d\d)/)[0]
+      return startTime
+    }
